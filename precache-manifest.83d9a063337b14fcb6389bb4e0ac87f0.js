eac5fb33a56099ed3f6d092b432fad7a1f2f7d32
Pierre Segerström <pise@kth.se>
2020-12-29 12:09:45 +0100

    Updates

---
+++ precache-manifest.83d9a063337b14fcb6389bb4e0ac87f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdddd5d23d27ae9e16fc15071e79415a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c92a019c38dcfc0df50c",
+    "url": "/portfolio/static/css/2.a3f71967.chunk.css"
+  },
+  {
+    "revision": "5831763722d9a2af9559",
+    "url": "/portfolio/static/css/main.2f17a264.chunk.css"
+  },
+  {
+    "revision": "c92a019c38dcfc0df50c",
+    "url": "/portfolio/static/js/2.42571ecd.chunk.js"
+  },
+  {
+    "revision": "5831763722d9a2af9559",
+    "url": "/portfolio/static/js/main.57561b87.chunk.js"
+  },
+  {
+    "revision": "c876296eb63c7d90bdee",
+    "url": "/portfolio/static/js/runtime~main.86960339.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
